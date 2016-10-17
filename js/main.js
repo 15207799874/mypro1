e270b19a3eedbc11ecda274915a9e40f2aa52e73
xuyang <289275357@qq.com>
2016-10-17 12:01:39 +0800

    10-17

--- js/main.js
+++ js/main.js
@@ -8,20 +8,31 @@ setTimeout(
 	function(){
 		var content1=$("#content1");				
 		if(content1.css("display")=="none"){
-			$("#content1").show("blind");
+			content1.show("blind");
+			$("#content2").hide();
 			setTimeout(
 				function(){
 					$("div.c1 h5").each(function(idx){$(this).fadeIn({duration:500*idx})});	
 					$("#fixnav").fadeIn(3000);},
 			2500);
+			$("#c2_p p").css("display","none");
 		}	
 				
 	}) 
  $("#btn2").on('click',
 	function(){
-		var content1=$("#content1");				
+		var content1=$("#content1");					
 		if(content1.css("display")!=="none"){
-			$("#content1").hide("blind");
+			content1.hide("blind");
+			$("#content2").show("blind");			
 			$("div.c1 h5").hide();
-		}				
+			$("#c2_p p").eq(0).delay(1000).show("blind",{direction:"left",duration:500},
+				function(){$(this).next().show("blind",{direction:"right",duration:500},
+					function(){$(this).next().show("blind",{direction:"left",duration:500},
+						function(){$(this).next().show("blind",{direction:"right",duration:500}
+						)}
+					)}
+				)}
+			)
+		}		
 	}) 
